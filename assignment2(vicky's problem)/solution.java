2e76989913e33388abd97ef01976f025fd77fa30
yoosuf14 <62879093+yoosuf14@users.noreply.github.com>
2022-03-01 23:56:26 +0530

    vickys problem
    started with vickys problem. Neet to finish the logic and provide output

---
+++ assignment2(vicky's problem)/solution.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.Scanner;
+class solution{
+	public static void calcJumpTime(int distance){
+			System.out.println(distance);
+		}
+	public static void main(String[] args) {
+
+		solution sol = new solution();
+				Scanner input = new Scanner(System.in);
+		int distance = input.nextInt();
+
+		sol.calcJumpTime(distance);
+
+		
+	}
+}
