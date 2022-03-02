2dfd7fd4719fc6403bd90b314354728560e7ead8
yoosuf14 <62879093+yoosuf14@users.noreply.github.com>
2022-03-02 23:29:53 +0530

    vickys problem update
    completed upto first round (15m)

--- assignment2(vicky's problem)/solution.java
+++ assignment2(vicky's problem)/solution.java
@@ -1,17 +1,37 @@
 import java.io.*;
 import java.util.Scanner;
 class solution{
-	public static void calcJumpTime(int distance){
-			System.out.println(distance);
+public int calcJumpTime(int distance){
+	//int timeElapsed = 15;
+
+		if(distance<=5){
+			return 0;
+		}
+		if(distance>5 && distance<9){
+			return 1;
+		}
+		if(distance == 9){
+			return 3;
+		}
+		if(distance>=10 && distance<=14){
+			return 6;
+		}
+		//if distance passes first round;
+		if(distance>15){
+			return 0;
+		}
+
+		return 0;
 		}
 	public static void main(String[] args) {
 
 		solution sol = new solution();
-				Scanner input = new Scanner(System.in);
+		Scanner input = new Scanner(System.in);
 		int distance = input.nextInt();
 
-		sol.calcJumpTime(distance);
+		int output = sol.calcJumpTime(distance);
 
+		System.out.println(output);
 		
 	}
 }
