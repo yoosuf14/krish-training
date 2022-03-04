07c128e7d7d9bf1bf036407ec959a5fa13fa43de
yoosuf14 <62879093+yoosuf14@users.noreply.github.com>
2022-03-04 22:08:05 +0530

    completed with bottom up approach
    working fine for all tested values

--- assignment2(vicky's problem)/solution.java
+++ assignment2(vicky's problem)/solution.java
@@ -2,48 +2,39 @@ import java.io.*;
 import java.util.Scanner;
 class solution{
 public int calcJumpTime(int distance){
-	//int timeElapsed = 15;
+	int timeElapsed = 0;
+	int distanceTravelled = 0;
 
-		if(distance<=5){
-			return 0;
-		}
-		if(distance>5 && distance<9){
-			return 1;
-		}
-		if(distance == 9){
-			return 3;
-		}
-		if(distance>=10 && distance<=14){
-			return 6;
-		}
-		//if distance passes first round;
-		if(distance>15){
-			int timeElapsed = 0;
-			timeElapsed += (distance/9)*6;
-			int partialRounds = distance%9;
-			if (partialRounds>0 && partialRounds<6) {
-				timeElapsed += 3;
-			}
-			if(partialRounds>=6 && partialRounds<=8){
+		while(distance>distanceTravelled){
+			distanceTravelled += 5;
+			if (distanceTravelled>=distance)
+				break;
 				timeElapsed++;
-			}
-			if (partialRounds>8 && partialRounds<9) {
+
+			distanceTravelled +=2;	
+			if (distanceTravelled>=distance) 
+				break;
 				timeElapsed += 2;
-			}
-			return timeElapsed-3;
+
+			distanceTravelled ++;	
+			if (distanceTravelled>=distance) 
+				break;
+				timeElapsed +=3;
+
+		}
+		return timeElapsed;
 		}
 
-		return 0;
-		}
 	public static void main(String[] args) {
 
 		solution sol = new solution();
 		Scanner input = new Scanner(System.in);
+		System.out.print("Please enter distance:");
 		int distance = input.nextInt();
 
 		int output = sol.calcJumpTime(distance);
 
-		System.out.println(output);
+		System.out.println("Time taken to travel the distance of "+distance+" is "+output);
 		
 	}
 }
