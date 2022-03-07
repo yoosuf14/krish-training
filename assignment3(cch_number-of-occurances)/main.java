4c6fc3607c6c266312dd1bb25f53c416afaf4873
yoosuf14 <62879093+yoosuf14@users.noreply.github.com>
2022-03-07 12:35:10 +0530

    created cch_3 and cch_4

---
+++ assignment3(cch_number-of-occurances)/main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class main{
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        char[] inp =  input.toCharArray();
+        int[] 
+        for(int i = 0; i<inp.length; i++){
+            int x = inp[i];
+            System.out.println(x);    
+        }
+    }
+}
